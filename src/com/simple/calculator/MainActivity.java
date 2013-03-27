5093ac52e75a79dc45b0b3efff1b1ebd498d20b6
siimeon <simohaakana@wireless-86-50-133-93.open.aalto.fi>
2013-03-27 14:53:39 +0200

    Control Update

--- src/com/simple/calculator/MainActivity.java
+++ src/com/simple/calculator/MainActivity.java
@@ -10,10 +10,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	public ArrayList<String> calculate;
-	public CharSequence calcuBuf;
-	public boolean calculated = true;
-	public boolean bracop = false;
+	public ArrayList<String> calculate = new ArrayList<String>();
+	public String buffer = null;
+	public String ans = "77";
 	TextView screen;
 	Button btn0;
 	Button btn1;
@@ -37,6 +36,10 @@ public class MainActivity extends Activity {
 	Button answer;
 	Button clear;
 	Button backSpace;
+	public static String POTENS = "²";
+	public static String SQROOT = "√";
+	public static String OBRACKET = "(";
+	public static String CBRACKET = ")";
 	
 	
 	
@@ -70,70 +73,156 @@ public class MainActivity extends Activity {
 		
 		
 	}
-	
-	public void addNumber(char a){
-		switch (a){
-		//TODO
-		case '+': break;
-		case '-': break;
-		case '/': break;
-		case '*': break;
-		default: break;
+	public void updScreen(){
+		if (this.calculate.size() == 0){ 
+			this.screen.setText("0");
+			return;
 		}
-		
+		String tmp = "";
+		for (String s : this.calculate) tmp = tmp + s;
+		this.screen.setText(tmp);
 	}
-	
 	public void don(View v){
-		if (this.calculated) {
-			calculated=false;
-			this.screen.setText("");
+		if (this.buffer == null){ 
+			if (calculate.size() == 0){
+				if ("ans".equals((String) v.getTag())){
+					buffer = ans;
+					calculate.add(buffer);
+				}
+				else{
+					buffer = (String) v.getTag();
+					calculate.add(this.buffer);
+				}
+			}
+			else if (calculate.get(this.calculate.size()-1).equals(POTENS) && calculate.size() != 0){
+				calculate.remove(calculate.size()-1);
+				buffer = calculate.get(calculate.size()-1);
+				buffer  = buffer + ( (String) v.getTag());
+				calculate.set(calculate.size()-1, buffer);
+			}
+			else {
+				if ("ans".equals((String) v.getTag())){
+					buffer = ans;
+					calculate.add(buffer);
+				}
+				else {
+					this.buffer = (String) v.getTag();
+					this.calculate.add(this.buffer);
+			
+				}
+			}
 		}
-		String tmp =  v.getTag().toString();
-		CharSequence t = this.screen.getText();
-		t = t.toString() + tmp;
-		this.screen.setText(t);
+		else {
+			if ( ((String) v.getTag()).equals(".") &&  buffer.contains(".")) return;
+			if ( ((String) v.getTag()).equals("ans")) return;
+			this.buffer  = this.buffer + ( (String) v.getTag() );
+			this.calculate.set(this.calculate.size()-1, this.buffer);
+		}
+		this.updScreen();
 	}
 	public void doact(View v){
-		String tmp = v.getTag().toString();
-		CharSequence t = this.screen.getText();
-		t = t.toString() + tmp;
-		this.screen.setText(t);
+		if (calculate.size() == 0) return;
+		if (this.buffer != null){
+			this.calculate.add((String) v.getTag());
+			buffer = null;
+			this.updScreen();
+			return;
+		}
+		else {
+			String tmp = this.calculate.get(this.calculate.size()-1);
+			if (tmp.equals(POTENS) || tmp.equals(CBRACKET)){
+				calculate.add((String) v.getTag());
+			}
+			else if (tmp.equals(SQROOT)) return;
+			else {
+				this.calculate.set(calculate.size()-1, (String) v.getTag());
+			}
+		}
+		this.updScreen();
 	}
 	public void clear(View v){
-		this.screen.setText("");
-		this.calculated = false;
+		this.calculate = new ArrayList<String>();
+		this.buffer = null;
+		this.updScreen();
 	}
 	public void erase(View v){
-		if (this.calculated == true){
-			this.screen.setText("");
-			this.calculated = false;
-			return;
+		if (calculate.size() == 0) return;
+ 		if (buffer != null){
+ 			if (buffer.length() != 1){
+ 				buffer = buffer.substring(0, buffer.length()-1);
+ 				calculate.set(calculate.size()-1, buffer);
+ 			}
+ 			else {
+ 				calculate.remove(calculate.size()-1);
+ 				buffer = null;
+ 			}
 		}
-		CharSequence t = this.screen.getText();
-		if (t.length() == 0) return;
-		t = t.subSequence(0, (t.length()-1));
-		this.screen.setText(t);
+		else {
+			String tmp = this.calculate.get(this.calculate.size()-1);
+			//if (tmp.equals(POTENS));
+			if (tmp.equals(SQROOT)){
+				calculate.remove(calculate.size()-1);
+			}
+			else if (tmp.equals(OBRACKET)){
+				calculate.remove(calculate.size()-1);
+			}
+			//else if (tmp.equals(CBRACKET));
+			else {
+				calculate.remove(calculate.size()-1);
+				tmp = this.calculate.get(this.calculate.size()-1);
+				if (tmp.equals(POTENS)) ;
+				else if (tmp.equals(CBRACKET)) ;
+				else buffer = tmp;
+			}
+		}
+		this.updScreen();
 	}
 	public void calc(View v){
-		if (this.calculated == true) return;
-		CharSequence t = this.screen.getText();
-		if (t.length() == 0) return;
-		t = t + "\n=";
-		this.screen.setText(t);
-		this.calculated = true;
+		//TODO jos laskutoimitus on viimeinen merkki niin se pitaa poistaa
 	}
 	public void brac(View v){
-		if (this.bracop == true){
-			CharSequence t = this.screen.getText();
-			t = t.toString() + ")";
-			this.screen.setText(t);
-			this.bracop = false;
-			return;
+		if (calculate.size() == 0){
+			calculate.add(OBRACKET);
 		}
-		this.bracop = true;
-		CharSequence t = this.screen.getText();
-		t = t.toString() + "(";
-		this.screen.setText(t);
+		else {
+			int open = 0;
+			int close = 0;
+			for (String st: calculate){
+				if (st.equals(OBRACKET)) open ++;
+				else if (st.equals(CBRACKET)) close++;
+			}
+			String tmp = calculate.get(calculate.size()-1);
+			if (buffer == null && tmp.compareTo(POTENS) != 0){
+				if (close < open && tmp.equals(CBRACKET)) calculate.add(CBRACKET);
+				else calculate.add(OBRACKET);
+			}
+			else if (buffer != null && close < open){
+				buffer = null;
+				calculate.add(CBRACKET);
+			}
+		}
+		this.updScreen();
+	}
+	public void tosecond(View v){
+		if (this.buffer == null ){
+			if (calculate.get(calculate.size()-1).equals(CBRACKET)){
+				calculate.add(POTENS);
+			}
+			else return;
+		}
+		else {
+			buffer = null;
+			calculate.add(POTENS);
+		}
+		this.updScreen();
+	}
+	public void squeroot(View v){
+		if (this.buffer != null) return;
+		if (calculate.size() != 0)
+			if (calculate.get(calculate.size()-1).equals(POTENS))
+				return;
+		calculate.add(SQROOT);
+		this.updScreen();
 	}
 	
 	@Override
