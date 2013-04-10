2cb7de97e3f5dd3b7bca2f95f2da76c166251636
siimeon <simohaakana@wireless-86-50-134-24.open.aalto.fi>
2013-04-10 14:07:23 +0300

    Revert "Equation solver"
    This reverts commit 25ad656d0aa3145c479290f3e44c1d00efcd02e6.

---
+++ src/com/simple/calculator/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.simple.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+	}
+
+}
